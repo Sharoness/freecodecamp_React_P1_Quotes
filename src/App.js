917ccd0b5d5bec014694bc53c08662a6c7d51df8
Sharoness <sharong1234@hotmail.com>
2020-05-11 14:29:50 +0200

    Added data

--- src/App.js
+++ src/App.js
@@ -6,103 +6,103 @@ import './App.css';
 const quotes = [
   {
     "img": "insert img",
-    "quote": "q",
-    "author": "q"
+    "quote": "\"If you want to get to know someone, find out what makes them angry.\"",
+    "author": "Gon Freecss"
   },
   {
     "img": "insert img",
-    "quote": "w",
-    "author": "w"
+    "quote": "\"If you want to get to know someone, find out what makes them angry.\"",
+    "author": "Gon Freecss"
   },
   {
     "img": "insert img",
-    "quote": "e",
-    "author": "e"
+    "quote": "\"Who wants to have their life planned out for them?\"",
+    "author": "Killua Zoldyck"
   },
   {
     "img": "insert img",
-    "quote": "r",
-    "author": "r"
+    "quote": "\"We didn't do anything... Our enemies did.\"",
+    "author": "Killua Zoldyck"
   },
   {
     "img": "insert img",
-    "quote": "t",
-    "author": "t"
+    "quote": "\"When I ignore a friend I have the ability to help, wouldn’t I be betraying him?\"",
+    "author": "Killua Zoldyck"
   },
   {
     "img": "insert img",
-    "quote": "y",
-    "author": "y"
+    "quote": "\"When I say it doesn’t hurt me, that means I can bear it.\"",
+    "author": "Killua Zoldyck"
   },
   {
     "img": "insert img",
-    "quote": "u",
-    "author": "u"
+    "quote": "\"The only principle is that there are no principles.\"",
+    "author": "Kurapika"
   },
   {
     "img": "insert img",
-    "quote": "i",
-    "author": "i"
+    "quote": "\"There are two things that collectors always want. The first is any item of extreme rarity. The second is colleagues to whom they can brag about their collection.\"",
+    "author": "Kurapika"
   },
   {
     "img": "insert img",
-    "quote": "o",
-    "author": "o"
+    "quote": "\"I want money – money can get you anything in this world.\"",
+    "author": "Leorio Paradinight"
   },
   {
     "img": "insert img",
-    "quote": "p",
-    "author": "p"
+    "quote": "\"For the right price, you can buy not only treasures, but dreams, hearts, and even people’s lives!\"",
+    "author": "Leorio Paradinight"
   },
   {
     "img": "insert img",
-    "quote": "a",
-    "author": "a"
+    "quote": "\"Human potential for evolution is limitless.\"",
+    "author": "Isaac Netero"
   },
   {
     "img": "insert img",
-    "quote": "s",
-    "author": "s"
+    "quote": "\"We are not desperate for help, we only seek the strong.\"",
+    "author": "Isaac Netero"
   },
   {
     "img": "insert img",
-    "quote": "d",
-    "author": "d"
+    "quote": "\"A prayer comes from the heart. If the heart achieves the correct form, it becomes emotion. And emotions may be manifested.\"",
+    "author": "Isaac Netero"
   },
   {
     "img": "insert img",
-    "quote": "f",
-    "author": "f"
+    "quote": "\"Love and hate are two sides of the same coin.\"",
+    "author": "Hisoka"
   },
   {
     "img": "insert img",
-    "quote": "g",
-    "author": "g"
+    "quote": "\"It takes a mere second for treasure to turn to trash.\"",
+    "author": "Hisoka"
   },
   {
     "img": "insert img",
-    "quote": "h",
-    "author": "h"
+    "quote": "\"Every step we take amends our fate.\"",
+    "author": "Hisoka"
   },
   {
     "img": "insert img",
-    "quote": "j",
-    "author": "j"
+    "quote": "\"You should enjoy the little detours to the fullest. Because that's where you'll find the things more important than what you want.\"",
+    "author": "Ging Freecss"
   },
   {
     "img": "insert img",
-    "quote": "k",
-    "author": "k"
+    "quote": "\"An apology is a promise to do things differently next time, and to keep the promise.\"",
+    "author": "Ging Freecss"
   },
   {
     "img": "insert img",
-    "quote": "l",
-    "author": "l"
+    "quote": "\"There are liars who only lie when there’s a reason to, and there are liars who also lie without a reason.\"",
+    "author": "Biscuit Krueger"
   },
   {
     "img": "insert img",
-    "quote": "z",
-    "author": "z"
+    "quote": "\"Skill is one thing, and caution another.\"",
+    "author": "Geretta"
   }
 ];
 
