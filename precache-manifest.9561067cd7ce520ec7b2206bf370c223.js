b2612bc3f36400ac17e646b33b8aad17e7691221
Sharoness <sharong1234@hotmail.com>
2020-09-01 11:43:07 +0100

    Updates

---
+++ precache-manifest.9561067cd7ce520ec7b2206bf370c223.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee03619a45ca2f806b3542e52c472ab",
+    "url": "/freecodecamp_React_P1_Quotes/index.html"
+  },
+  {
+    "revision": "549553e90c57f1f8158b",
+    "url": "/freecodecamp_React_P1_Quotes/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "74b254cf4ed25a247ed3",
+    "url": "/freecodecamp_React_P1_Quotes/static/css/main.876b8ab0.chunk.css"
+  },
+  {
+    "revision": "549553e90c57f1f8158b",
+    "url": "/freecodecamp_React_P1_Quotes/static/js/2.81eb4a2d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/freecodecamp_React_P1_Quotes/static/js/2.81eb4a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b254cf4ed25a247ed3",
+    "url": "/freecodecamp_React_P1_Quotes/static/js/main.7e7a9bde.chunk.js"
+  },
+  {
+    "revision": "8ba50d84adcb80f02eb4",
+    "url": "/freecodecamp_React_P1_Quotes/static/js/runtime-main.a2574618.js"
+  },
+  {
+    "revision": "cb3eaeea0c251692ca2a9d2323507475",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/Biscuit_Full_Body_View.cb3eaeea.jpg"
+  },
+  {
+    "revision": "d46cc87e42cc221dfe4b61d143904b7a",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/Killua_murders_bodoro.d46cc87e.png"
+  },
+  {
+    "revision": "bbdb35e0d1230224ec5736ff5b40921e",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/Netero-Debut.bbdb35e0.jpg"
+  },
+  {
+    "revision": "90c6f24ecf8eaf691f4dd66367a2799e",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/geretta.90c6f24e.png"
+  },
+  {
+    "revision": "485eae4a6fc77547f6b1e0eba682bb67",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/gingfreecss.485eae4a.jpg"
+  },
+  {
+    "revision": "894271942862358744322c3e07984fb1",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/gonDetermined.89427194.jpg"
+  },
+  {
+    "revision": "2e4a32109317e4e94e16b03b97d07d5b",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/gonSmiling.2e4a3210.png"
+  },
+  {
+    "revision": "1f4c72fae7d796743bf810ea188e5a9c",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/gon_and_killua.1f4c72fa.jpg"
+  },
+  {
+    "revision": "d034e103d7ab0acb1ab5b46a123bc68c",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/hisoka_eye.d034e103.jpg"
+  },
+  {
+    "revision": "e2f5a6c4854df051617d412bc8693b42",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/hisoka_hairdown.e2f5a6c4.png"
+  },
+  {
+    "revision": "ed598ed7ffac8952de53ba913f0d0e5c",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/killua_bored.ed598ed7.jpg"
+  },
+  {
+    "revision": "ca79d0ffc6f11bdea8b21f20d75a84fa",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/killua_godspeed.ca79d0ff.png"
+  },
+  {
+    "revision": "b8608130cd249c4a31bc917355824b39",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/kurapika.b8608130.jpg"
+  },
+  {
+    "revision": "746add74b17d9974686435e49b28fbd3",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/kurapika_scarlet_eyes.746add74.jpg"
+  },
+  {
+    "revision": "c14400fb86fae71ca4232cd4b7257989",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/leorio.c14400fb.jpg"
+  },
+  {
+    "revision": "e8439b13f583c2d77cf7bd3c30204c21",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/leorio_money.e8439b13.jpg"
+  },
+  {
+    "revision": "1094a50aae790737288cbbb519a29d4d",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/neteroNen.1094a50a.jpg"
+  },
+  {
+    "revision": "927536033bd68b10ce87deca7961d392",
+    "url": "/freecodecamp_React_P1_Quotes/static/media/netero_ball.92753603.jpg"
+  }
+]);
